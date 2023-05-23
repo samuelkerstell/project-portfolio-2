c544ac4732debb902e85b1ef998ffeb79626ea68
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-05-23 18:39:38 +0000

    Add javascript to the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-btns')
+const answerButtonsElement = document.getElementById('answer')
 
 
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
-    console.log('Started')
     startButton.classList.add('hide')
     shuffledQuestions = QUESTIONS.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
@@ -18,15 +22,59 @@ function startGame() {
 }
 
 function setNextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question
+    question.answer.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 }
 
-function selectAnswer() {
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(event) {
+    const selectedButton = event.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
     
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 const QUESTIONS = [
@@ -38,5 +86,14 @@ const QUESTIONS = [
         { text: 'Manchester United', correct: true },
         { text: 'Manchester City', correct: false },
     ]
-    }
+    },
+    {
+        question: "Which is the only team who has won the Premier League without losing a single game?",
+        answer: [
+           { text: 'Manchester United', correct: false },
+           { text: 'Liverpool', correct: false },
+           { text: 'Arsenal', correct: true },
+           { text: 'Chelsea', correct: false },
+       ]
+       },
 ]
